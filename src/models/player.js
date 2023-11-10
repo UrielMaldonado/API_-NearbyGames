45e5e04a116d9d9429557c24eef6582ae78f6045
UrielMaldonado <uri.7ma.3co@gmail.com>
2023-11-10 14:21:58 -0600

    player model sql comentario insert

--- src/models/player.js
+++ src/models/player.js
@@ -32,3 +32,11 @@ const Player= dbconection.define("tbb_players",
 }
 )
 export default Player
+
+/* use db_200931;
+
+INSERT INTO tbb_players (name, email, password, nickname, birthday, portrait_img, createdAt, updatedAt)
+VALUES ('John Doe', 'johndoe@example.com', 'password123', 'johndoe', '1990-01-01 00:00:00', 'portrait.jpg', '2023-11-10 20:16:12', '2023-11-10 20:16:12'),
+       ('Jane Doe', 'janedoe@example.com', 'password456', 'janedoe', '1992-01-01 00:00:00', 'portrait.jpg', '2023-11-10 20:16:12', '2023-11-10 20:16:12'),
+       ('Bob Smith', 'bobsmith@example.com', 'password789', 'bobsmith', '1985-01-01 00:00:00', 'portrait.jpg', '2023-11-10 20:16:12', '2023-11-10 20:16:12');
+ */
